c3ac710fb2c754b77422edcdad5df45d882572d5
roaa-barq <roaa.barq@gmail.com>
2023-05-22 14:50:08 +0300

    roaa

--- routers/user_routers.js
+++ routers/user_routers.js
@@ -1,25 +1,24 @@
 const router = require("express").Router();
-const UserController = require('../controllers/user_controller');
-const multer = require('multer');
+const UserController = require("../controllers/user_controller");
+const multer = require("multer");
 
 const storage = multer.memoryStorage(); //for pdf resume
 const upload = multer({ storage });
 
-
 //CRUD OPS on jobs
-router.post("/addJob",UserController.addJob);  //~~~~~~~create
+router.post("/addJob", UserController.addJob); //~~~~~~~create
 
+router.post("/apply", upload.single("resume"), UserController.addApplication); ///aply for a job (post) --create
+router.get("/applicaions/:id", UserController.getApplicationById); //~~~~~~~~retrieve
+router.put("/applicaions/:id", UserController.updateApplicationOfId); //~~~~~update
+router.delete("/applicaions/:id", UserController.deleteApplication); // ~~~~delete
 
+router.get("/applications", UserController.getAllApplications); ///get applications
+router.get(
+  "/applications/:id/resume",
+  UserController.getResumeOfSpecificApplication
+); // get the resume and download for a specific application
 
-
-
-
-router.post('/apply', upload.single('resume'), UserController.addApplication); ///aply for a job (post) --create
-
-
-
-router.get('/applications',UserController.getAllApplications);                 ///get applications 
-router.get('/applications/:id/resume',UserController.getResumeOfSpecificApplication );  // get the resume and download for a specific application
-
+router.get("/applications/:email", UserController.getMyApplicationsByEmail);
 
 module.exports = router;
--- services/user_services.js
+++ services/user_services.js
@@ -1,21 +1,73 @@
 const ApplicationModel = require("../models/application_model");
 const JobModel = require("../models/job_model");
+const SavedJobModel = require("../models/saved_job_model");
 
-class UserServices{
- 
-    static async addJob( jobTitle, description, requirements,location,salary){
-        try{
-               //console.log("-----job name-----",jobName);
-                
-                const createJob = new JobModel({ jobTitle, description, requirements,location,salary});
-                return await createJob.save();
-        }catch(err){
-            throw err;
-        }
+class UserServices {
+  static async addJob(jobTitle, description, requirements, location, salary) {
+    try {
+      //console.log("-----job name-----",jobName);
+
+      const createJob = new JobModel({
+        jobTitle,
+        description,
+        requirements,
+        location,
+        salary,
+      });
+      return await createJob.save();
+    } catch (err) {
+      throw err;
     }
-   
-  
-   
+  }
+  static async addSavedJob(jobId) {
+    try {
+      //console.log("-----job name-----",jobName);
+
+      const createJob = new SavedJobModel({ jobId });
+      return await createJob.save();
+    } catch (err) {
+      throw err;
+    }
+  }
+  static async getJobs() {
+    const dataList = await JobModel.findAll();
+    return dataList;
+  }
+  static async getSavedJobs() {
+    const dataList = await SavedJobModel.find().populate("jobId");
+    return dataList;
+  }
+  static async getJobById(id) {
+    const job = await JobModel.findById(id);
+    return job;
+  }
+  static async updateJobOfId(req) {
+    const job = JobModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    // console.log()
+    return job;
+  }
+  static async deleteJob(req) {
+    const job = JobModel.findByIdAndRemove(req.params.id);
+    return job;
+  }
+  /*******************applications crud */
+  static async getApplicationById(id) {
+    const job = await ApplicationModel.findById(id);
+    return job;
+  }
+  static async updateApplicationOfId(req) {
+    const job = ApplicationModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    // console.log()
+    return job;
+  }
+  static async deleteApplication(req) {
+    const job = ApplicationModel.findByIdAndRemove(req.params.id);
+    return job;
+  }
 }
 
 module.exports = UserServices;
